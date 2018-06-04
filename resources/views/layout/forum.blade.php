ecf0cdb818e42ac1344c9de3c8bbcda21a1edac9
aquintanilla1 <alvaro.quintanilla@stonybrook.edu>
2018-06-04 15:54:25 -0400

    Added login page, playing around with yields.

---
+++ resources/views/layout/forum.blade.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    @yield('title')
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
---
+++ resources/views/pages/login.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.forum)
+
+@section('title')
+    <title>The Forum: Log In</title>
+@endsection
+
+@section('content')
+    <h1>Log In Here</h1>
+    <body>
+        <p>
+            Some random stuff.
+        </p>
+    </body>
