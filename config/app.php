8d4d00e57524eb430982dde48f4484e102e1e120
aquintanilla1 <qtptaquintanilla@gmail.com>
2018-06-07 02:34:48 +0000

    Hoping that changing these files for for forms

--- config/app.php
+++ config/app.php
@@ -160,7 +160,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-	//Collective\Html\HtmlServiceProvider::class,
+	Collective\\Html\\HtmlServiceProvider::class,
 
     ],
 
@@ -211,8 +211,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
-//	'Form' => Collective\Html\FormFacade::class,
-//	'Html' => Collective\Html\HtmlFacade::class,
+	'Form' => Collective\\Html\\FormFacade::class,
+	'Html' => Collective\\Html\\HtmlFacade::class,
     ],
 
 ];
