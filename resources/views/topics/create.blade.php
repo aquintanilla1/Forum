5bc4b15ce84aa062982f1965a6f4c4a884a325c7
aquintanilla1 <alvaro.quintanilla@stonybrook.edu>
2018-06-08 00:43:42 -0400

    Added submit button

--- resources/views/topics/create.blade.php
+++ resources/views/topics/create.blade.php
@@ -9,16 +9,17 @@
 
     {!! Form::open(['action' => 'TopicsController@store', 'method' => 'POST']) !!}
 
-    {{Form::label('topicTitle', 'Title')}}
-    {{Form::text('topicTitle', 'Enter Title')}}
-    {{Form::label('titleLength', 'max 255 char.')}}
-
-    <br>
-
+    <p> {{Form::label('topicTitle', 'Title')}}
+        {{Form::text('topicTitle', 'Enter Title')}}
+        {{Form::label('titleLength', 'max 255 char.')}}
+    </p>
+    <p>
     {{Form::label('topicBody', 'Body')}}
     {{Form::textarea('topicBody', 'Enter Body')}}
     {{Form::label('bodyLength', 'max 1000 char.')}}
+    </p>
 
+    {{Form::submit('Submit Topic')}}
 
     {!! Form::close() !!}
 @endsection
