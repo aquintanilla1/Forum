55554beeb2fa3cb7569e6309b43ceef78f2c94e1
aquintanilla1 <alvaro.quintanilla@stonybrook.edu>
2018-06-09 15:49:33 -0400

    Testing stuff to get around BS CSS.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,9 @@
 @extends('layout.forum')
 
+@section('title')
+    <title>{{$title}}: Log In</title>
+
+@endsection
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
