40c86285a2df39de2b301366ccecbc71302191c1
aquintanilla1 <alvaro.quintanilla@stonybrook.edu>
2018-06-08 21:21:29 -0400

    Fixing edit functionality bugs.

--- resources/views/topics/edit.blade.php
+++ resources/views/topics/edit.blade.php
@@ -11,12 +11,12 @@
     {!! Form::open(['action' => ['TopicsController@update', $topic->id], 'method' => 'POST']) !!}
 
     <p> {{Form::label('topicTitle', 'Title')}}
-        {{Form::text('topicTitle', '$topic->topicTitle')}}
+        {{Form::text('topicTitle', $topic->topicTitle)}}
         {{Form::label('titleLength', 'max 255 char.')}}
     </p>
     <p>
         {{Form::label('topicBody', 'Body')}}
-        {{Form::textarea('topicBody', '$topic->topicBody')}}
+        {{Form::textarea('topicBody', $topic->topicBody}}
         {{Form::label('bodyLength', 'max 1000 char.')}}
     </p>
     {{Form::hidden('_method', 'PUT')}}
