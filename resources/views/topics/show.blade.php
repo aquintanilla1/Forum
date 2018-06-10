c1919eb2e6b5e6587d3f8ef60e7adc65203286e2
aquintanilla1 <alvaro.quintanilla@stonybrook.edu>
2018-06-10 18:48:43 -0400

    Authors of topics listed on individual topic pages

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
     <h2>{{ $topic->topicTitle }}</h2>
-    <h3>Posted on {{$topic->created_at}}</h3>
+    <h3>Posted on {{$topic->created_at}} by {{ $topic->user['name'] }}</h3>
 
     <p>{{ $topic->topicBody }}</p>
 
